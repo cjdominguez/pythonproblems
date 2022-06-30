bd0c98b89ca2d7d629a9fad1d82980c7dad7edb7
Christian Dominguez <cjd1123@gmail.com>
2022-06-30 09:50:36 -0400

    problems from Zero to Hero - makes twenty problem

---
+++ pythonBootcamp_Zero2Hero_Udemy/makesTwenty.py
@@ -0,0 +1,24 @@
+"""
+MAKES TWENTY: Given two integers, return True if the sum of the integers is 20
+ or if one of the integers is 20. If not, return False
+
+makes_twenty(20,10) --> True
+makes_twenty(12,8) --> True
+makes_twenty(2,3) --> False
+
+"""
+
+def makes_twenty(a, b):
+    """
+    check if a or b is #20
+    if so return True
+
+    else then,
+
+    if sum of a and b is 20 return True
+
+    else False
+    """
+
+
+    pass
