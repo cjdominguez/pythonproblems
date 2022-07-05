95a9b81e86f94500fa7e64f78097207807eab888
Christian Dominguez <cjd1123@gmail.com>
2022-07-05 13:32:38 -0400

    problems from Zero to Hero - find)thirty_three.py
    search array to find [3,3] in the index and return True or False if T/F

---
+++ pythonBootcamp_Zero2Hero_Udemy/find_thirty_three.py
@@ -0,0 +1,47 @@
+"""
+Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
+
+has_33([1, 3, 3]) → True
+has_33([1, 3, 1, 3]) → False
+has_33([3, 1, 3]) → False
+
+###################################################################################
+
+we have a list of nums
+
+last_iterator =
+traverse the list r to L
+storing the last iterator
+
+    if last iterator equals new iterator
+
+    return True
+
+    else false
+
+
+###################################################################################
+
+"""
+
+
+def has_33(nums):
+    for i in range(0, len(nums) - 1):
+        if nums[i] == 3 and nums[i + 1] == 3:
+            return True
+    return False
+
+    """
+    for i in range(0, len(nums) - 1):
+
+        # nicer looking alternative in commented code
+        # if nums[i] == 3 and nums[i+1] == 3:
+
+        if nums[i:i + 2] == [3, 3]:
+            return True
+
+    return False
+    """
+
+
+print(has_33([1, 2, 0, 2, 1, 2, 4, 4, 3, 1, 3, 3]))
